52c38cabfc6cad16757c468334349e7e034a72e6
tmek1244 <tmek1244@wp.pl>
2023-08-05 23:44:33 +0200

    Add basic settings

--- config.lua
+++ config.lua
@@ -2,3 +2,27 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+
+lvim.leader = "space"
+
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+lvim.lang.python.formatter = {
+  {
+    exe = "black",
+  }
+}
+
+lvim.lang.python.linters = {
+  {
+    exe = "flake8",
+  }
+}
+
+lvim.plugins = {
+  {},
+}
